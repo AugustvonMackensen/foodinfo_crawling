f47d62af095bd17a6af2fac12270f4afda6b8fad
AugustvonMackensen <bonaparte_19th@hotmail.com>
2022-11-08 14:06:24 +0900

    regional traditional korean food crawling

--- foodinfo_crawling.py
+++ foodinfo_crawling.py
@@ -3,44 +3,60 @@ from selenium import webdriver as wd
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
+driver = wd.Chrome(service=Service(ChromeDriverManager().install()))
+
+food_dict = {}
 
 def run():
+    # 전통향토음식 음식정보 크롤링
     # selenium 업그레이드 된 이후 driver 등록 코드
     # chromedriver.exe 굳이 프로젝트 폴더에 안 넣어도 됨
+    # 가나다순으로 크롤링, 페이징 관련 문제로 있어 ㄱ~ㅎ으로 나눠서 처리.
+    get_item(27, 'ㄱ')
+    get_item(4, 'ㄴ')
+    get_item(18, 'ㄷ')
+    get_item(18, 'ㅁ')
+    get_item(14, 'ㅂ')
+    get_item(20, 'ㅅ')
+    get_item(20, 'ㅇ')
+    get_item(12, 'ㅈ')
+    get_item(7, 'ㅊ')
+    get_item(4, 'ㅋ')
+    get_item(2, 'ㅌ')
+    get_item(4, 'ㅍ')
+    get_item(9, 'ㅎ')
 
-    driver = wd.Chrome(service=Service(ChromeDriverManager().install()))
-
-    for page in range(1, 155):
-        # 접속할 사이트 url 연결
-        main_url = 'http://terms.naver.com/list.naver?cid=42701&categoryId=42708&so=st3.asc&viewType=&categoryType=&index=%E3%84%B1&page=' \
-                   + str(page) + "'"
-
-        food_dict = {}
-        food_list = []
-        # 사이트 접속
-        driver.get(main_url)
-        info_elements = driver.find_elements(By.CSS_SELECTOR, 'div.list_wrap > ul > li')
-        info_num = len(info_elements)
+    print(food_dict)
+# 페이지 최종 숫자와 한글 자음 입력받아 메소드 실행
+def get_item(n, ch):
+    for page in range(1, n+1):
         food_title = ""
         food_info = ""
+        # 사이트 접속
+        url = 'https://terms.naver.com/list.naver?cid=42701&categoryId=42708&so=st3.asc&viewType=&categoryType= \
+              &index='+ ch + '&page='+ n + "'"
+        driver.get(url)
+        info_elements = driver.find_elements(By.CSS_SELECTOR, 'div.list_wrap > ul > li')
+        info_num = len(info_elements)
         for item in range(1, info_num + 1):
             try:
+                # selenium.common.exceptions.ElementClickInterceptedException 방지위해 send_keys() 메소드 사용
                 driver.find_element(By.CSS_SELECTOR, 'div.list_wrap > ul > li:nth-child(' + str(item) + ') > div > \
-                                    div.subject > strong > a:nth-child(1)').click()
-
-                print(driver.find_element(By.CSS_SELECTOR, 'div.section_wrap > div.headword_title > h2').text)
-                print(driver.find_element(By.CSS_SELECTOR, '#size_ct > p').text)
-
+                                    div.subject > strong > a:nth-child(1)').send_keys(Keys.ENTER)
                 food_title = driver.find_element(By.CSS_SELECTOR, 'div.section_wrap > div.headword_title > h2').text
                 food_info = driver.find_elements(By.CSS_SELECTOR, '#size_ct > p')
-                food_dict[food_title] = food_info
                 driver.back()  # 뒤로 가기
+                if(item == info_num):
+                    break
             except NoSuchElementException:
                 driver.back()
                 continue
+            else:
+                food_dict[food_title] = food_info
 
 
