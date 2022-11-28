824647fdb49d157519189ecae977f6ed66f041a1
Se Min Lee <bonaparte_19th@hotmail.com>
2022-11-29 01:33:10 +0900

    changed code

--- foodinfo_crawling.py
+++ foodinfo_crawling.py
@@ -1,9 +1,11 @@
+import time
+
 from selenium.common import NoSuchElementException
 from selenium import webdriver as wd
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.keys import Keys
+import os
 
 driver = wd.Chrome(service=Service(ChromeDriverManager().install()))
 
@@ -13,51 +15,41 @@ def run():
     # 전통향토음식 음식정보 크롤링
     # selenium 업그레이드 된 이후 driver 등록 코드
     # chromedriver.exe 굳이 프로젝트 폴더에 안 넣어도 됨
-    # 가나다순으로 크롤링, 페이징 관련 문제로 있어 ㄱ~ㅎ으로 나눠서 처리.
-    get_item(27, 'ㄱ')
-    get_item(4, 'ㄴ')
-    get_item(18, 'ㄷ')
-    get_item(18, 'ㅁ')
-    get_item(14, 'ㅂ')
-    get_item(20, 'ㅅ')
-    get_item(20, 'ㅇ')
-    get_item(12, 'ㅈ')
-    get_item(7, 'ㅊ')
-    get_item(4, 'ㅋ')
-    get_item(2, 'ㅌ')
-    get_item(4, 'ㅍ')
-    get_item(9, 'ㅎ')
 
-    return food_dict
+    folder_list =[]
+    ignored_list = []
+    dir = os.listdir('C:/finalProject_datasets/Training')
+    for folder in dir:
+        folder_list.append(folder)
 
-# 페이지 최종 숫자와 한글 자음 입력받아 메소드 실행
-def get_item(n, ch):
-    for page in range(1, n+1):
-        food_title = ""
-        food_info = ""
+    print(folder_list)
+    # 사이트 접속
+
+    for item in range(len(folder_list)):
         # 사이트 접속
-        url = 'https://terms.naver.com/list.naver?cid=42701&categoryId=42708&so=st3.asc&viewType= \
-              &categoryType=&index='+ ch + '&page='+ str(page) + "'"
+        url = 'https://terms.naver.com/'
         driver.get(url)
-        info_elements = driver.find_elements(By.CSS_SELECTOR, 'div.list_wrap > ul > li')
-        info_num = len(info_elements)
-        for item in range(1, info_num + 1):
-            try:
-                # selenium.common.exceptions.ElementClickInterceptedException 방지위해 send_keys() 메소드 사용
-                driver.find_element(By.CSS_SELECTOR, 'div.list_wrap > ul > li:nth-child(' + str(item) + ') > div > \
-                                    div.subject > strong > a:nth-child(1)').send_keys(Keys.ENTER)
-                food_title = driver.find_element(By.CSS_SELECTOR, 'div.section_wrap > div.headword_title > h2').text
-                # 개행문자 제거
-                food_info = driver.find_element(By.CSS_SELECTOR, '#size_ct > p').text.replace('\n', '')
-                driver.back()  # 뒤로 가기
-                if(item == info_num):
-                    break
-            except NoSuchElementException:
-                driver.back()
-                continue
-            else:
-                # 음식설명이 있는 데이터만 딕셔너리에 저장
-                if food_info is not None:
-                    food_dict[food_title] = food_info
+
+        keyword = folder_list[item]
+        time.sleep(5)
+        try:
+            driver.find_element(By.CSS_SELECTOR, '#term_query').send_keys(keyword)
+            driver.find_element(By.CSS_SELECTOR, '#terms_search_form > fieldset > div > input.btn_search._search').click()
+            driver.find_element(By.CSS_SELECTOR, '#content > div > ul > li > div > div > strong > a').click()
+            food_title = keyword
+            # 개행문자 제거
+            food_content = driver.find_element(By.CSS_SELECTOR, '#size_ct > p').text.replace('\n', '')
+            food_dict[food_title] = food_content
+            print(keyword + ': ' + food_content)
+            driver.back()
+        except NoSuchElementException:
+            print('누락될 키워드 : ' + keyword)
+            ignored_list.append(keyword)
+            pass
+
+
+    print(food_dict)
+
+
 
 
--- main.py
+++ main.py
@@ -2,19 +2,20 @@ import foodinfo_crawling as crawling
 import common.oracle_db as oradb
 
 if __name__ == '__main__':
-    food_dict = crawling.run()
-    oradb.oracle_init()
-    conn = oradb.connect()
-    cursor = conn.cursor()
-
-    query = 'insert into foodinfo(food_name, food_content) values(%s, %s)'
-
-    foodname_list = list(food_dict.keys())
-    foodinfo_list = list(food_dict.items())
-
-    for item in range(len(food_dict)):
-        foodinfo_val = (foodname_list[item], foodinfo_list[item])
-        cursor.execute(query, foodinfo_val)
-
-    print(food_dict)
+    crawling.run()
+    # food_dict = crawling.run()
+    # oradb.oracle_init()
+    # conn = oradb.connect()
+    # cursor = conn.cursor()
+    #
+    # query = 'insert into foodinfo(food_name, food_content) values(%s, %s)'
+    #
+    # foodname_list = list(food_dict.keys())
+    # foodinfo_list = list(food_dict.items())
+    #
+    # for item in range(len(food_dict)):
+    #     foodinfo_val = (foodname_list[item], foodinfo_list[item])
+    #     cursor.execute(query, foodinfo_val)
+    #
+    # print(food_dict)
 
