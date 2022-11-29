185791387664b4e3225564d7311f454cc5cc18d5
Se Min Lee <bonaparte_19th@hotmail.com>
2022-11-29 11:46:59 +0900

    메인 코드 수정

--- main.py
+++ main.py
@@ -2,19 +2,27 @@ import foodinfo_crawling as crawling
 import common.oracle_db as oradb
 
 if __name__ == '__main__':
-    crawling.run()
     food_dict = crawling.run()
-    oradb.oracle_init()
-    conn = oradb.connect()
-    cursor = conn.cursor()
+    conn = ''
+    cursor = ''
+    try:
+        oradb.oracle_init()
+        conn = oradb.connect()
+        cursor = conn.cursor()
 
-    query = 'insert into foodinfo(food_name, food_content) values(%s, %s)'
-    foodname_list = list(food_dict.keys())
-    foodinfo_list = list(food_dict.items())
+        query = 'insert into foodinfo(food_name, food_content) values(%s, %s)'
+        foodname_list = list(food_dict.keys())
+        foodinfo_list = list(food_dict.items())
 
-    for item in range(len(food_dict)):
-        foodinfo_val = (foodname_list[item], foodinfo_list[item])
-        cursor.execute(query, foodinfo_val)
-
-    print(food_dict)
+        for item in range(len(food_dict)):
+            foodinfo_val = (foodname_list[item], foodinfo_list[item])
+            cursor.execute(query, foodinfo_val)
+            oradb.commit(conn)
+    except Exception as msg:
+        oradb.rollback(conn)
+        print(conn)
+        print('크롤링 데이터 삽입 실패 : ', msg)
+    finally:
+        cursor.close()
+        oradb.close(conn)
 
