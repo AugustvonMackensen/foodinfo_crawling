896cb1363d3909996d79c514fff6232f3d776cca
Se Min Lee <bonaparte_19th@hotmail.com>
2022-11-29 11:29:39 +0900

    코드 수정 및 json 파일 추가

--- foodinfo_crawling.py
+++ foodinfo_crawling.py
@@ -1,4 +1,5 @@
 import time
+import json
 
 from selenium.common import NoSuchElementException
 from selenium import webdriver as wd
@@ -16,25 +17,21 @@ def run():
     # selenium 업그레이드 된 이후 driver 등록 코드
     # chromedriver.exe 굳이 프로젝트 폴더에 안 넣어도 됨
 
-    folder_list =['가래떡', '가지구이', '갈비구이', '갈비찜', '갈비탕', '개피떡', '게장', '경단', '계란말이', '고갈비',
-                  '고등어구이', '고추장불고기', '곱창전골', '국수', '김밥', '김치찌개', '낚지볶음', '냉면', '닭갈비', '닭찜',
-                  '돼지갈비찜', '돼지고기구이', '돼지고기볶음', '된장국', '된장찌개', '떡갈비', '떡만둣국', '떡볶이', '만두',
-                  '만둣국', '명란젓', '배추김치', '보쌈', '부대찌개', '북엇국', '삼겹살구이', '삼계탕', '새우구이', '송편', '순대',
-                  '순대국밥', '순대볶음', '순두부찌개', '아귀찜', '알밥', '양념치킨', '오리구이', '오이소박이', '왕만두',
-                  '유과', '육포', '육회', '육회비빔밥', '잡채', '장어구이', '족발', '주먹밥', '죽', '찐빵', '찹쌀떡',
-                  '총각김치', '추어탕', '콩국수', '통닭', '파전', '팥빙수', '팥죽', '해물찜', '해물탕', '해장국',
-                  '호두과자', '훈제오리']
+    data = open('./kor_eng_foodtitle.json', encoding='utf-8').read()
+    rom_dict = json.loads(data)
+    print(rom_dict)
 
+    kor_title = list(rom_dict.values())
 
-    print(folder_list)
+    print(kor_title)
     # 사이트 접속
 
-    for item in range(len(folder_list)):
+    for item in range(len(kor_title)):
         # 사이트 접속
         url = 'https://terms.naver.com/'
         driver.get(url)
 
-        keyword = folder_list[item]
+        keyword = kor_title[item]
         time.sleep(5)
         try:
             driver.find_element(By.CSS_SELECTOR, '#term_query').send_keys(keyword)
--- main.py
+++ main.py
@@ -3,19 +3,18 @@ import common.oracle_db as oradb
 
 if __name__ == '__main__':
     crawling.run()
-    # food_dict = crawling.run()
-    # oradb.oracle_init()
-    # conn = oradb.connect()
-    # cursor = conn.cursor()
-    #
-    # query = 'insert into foodinfo(food_name, food_content) values(%s, %s)'
-    #
-    # foodname_list = list(food_dict.keys())
-    # foodinfo_list = list(food_dict.items())
-    #
-    # for item in range(len(food_dict)):
-    #     foodinfo_val = (foodname_list[item], foodinfo_list[item])
-    #     cursor.execute(query, foodinfo_val)
-    #
-    # print(food_dict)
+    food_dict = crawling.run()
+    oradb.oracle_init()
+    conn = oradb.connect()
+    cursor = conn.cursor()
+
+    query = 'insert into foodinfo(food_name, food_content) values(%s, %s)'
+    foodname_list = list(food_dict.keys())
+    foodinfo_list = list(food_dict.items())
+
+    for item in range(len(food_dict)):
+        foodinfo_val = (foodname_list[item], foodinfo_list[item])
+        cursor.execute(query, foodinfo_val)
+
+    print(food_dict)
 
